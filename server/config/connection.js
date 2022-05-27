2f44457bcb9b7acce37427fef9adc92bcd3099ed
autumnlf <autumnF12@gmail.com>
2022-05-26 23:49:41 -0400

    fixed db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-book-search-engine-21', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
