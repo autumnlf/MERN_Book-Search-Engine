13eaae0e16fbf516a67a882e92c22bcba6a1cf0f
autumnlf <autumnF12@gmail.com>
2022-05-31 00:08:00 -0400

    fixing db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookDB/bookstuff', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
